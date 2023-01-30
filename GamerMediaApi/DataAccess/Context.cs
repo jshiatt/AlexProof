335d3be7ee6d6889cc550a4d1aee5e3dc2f055a2
jshiatt <jhiatt@spotinc.com>
2023-01-30 01:49:55 -0500

    big push

---
+++ GamerMediaApi/DataAccess/Context.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
